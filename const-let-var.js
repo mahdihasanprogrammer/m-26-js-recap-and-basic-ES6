7fbf12308ae270ed891014a999d8b38eb04ef659
mahdihasanprogrammer <mahdihasan.code@gmail.com>
2026-02-26 07:35:25 +0600

    added deffrence of between var, let, const

---
+++ const-let-var.js
@@ -0,0 +1,23 @@
+
+// console.log(a)
+var a = 20;
+var a= 50;
+// console.log(a)
+
+
+function num (){
+    var b = 200;
+    console.log(b)
+}
+
+// console.log(b);
+
+if(true){
+    var x =20
+}
+console.log(x)
+
+// console.log(country)
+let country;
+
+
